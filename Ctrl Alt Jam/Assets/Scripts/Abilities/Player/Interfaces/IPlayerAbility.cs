05b30a040d131e45d42ac9312bc1c5d8f51e8de0
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-05-15 17:44:57 -0300

    Added more things to IPlayerInterface to handle event

--- Ctrl Alt Jam/Assets/Scripts/Abilities/Player/Interfaces/IPlayerAbility.cs
+++ Ctrl Alt Jam/Assets/Scripts/Abilities/Player/Interfaces/IPlayerAbility.cs
@@ -1,11 +1,18 @@
+using System;
 
 namespace JAM.Abilites
 {
     public interface IPlayerAbility
     {
+        public event Action<AbilityType> OnActivateAbility;
+        public AbilityType Type { get; }
+        public AbiliteState State { get; }
+
+        public void ResetAbility();
         public void SetInput(PlayerInputActions actions);
         public void Enter();
         public void HandleInput();
+        public void HandleAbilityInLoop();
         public void HandleAbility();
         public void PhysicsUpdate();
         public void Exit();
