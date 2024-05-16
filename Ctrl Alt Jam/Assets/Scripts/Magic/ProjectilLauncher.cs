fdb4794a5f144467d8c6ff261f13cb00f46f11de
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-05-16 10:41:17 -0300

    rename magic launcher script

--- Ctrl Alt Jam/Assets/Scripts/Magic/ProjectilLauncher.cs
+++ Ctrl Alt Jam/Assets/Scripts/Magic/ProjectilLauncher.cs
@@ -3,18 +3,21 @@ using JAM.Characters;
 
 namespace JAM.Projectils
 {
-    public class MagicLauncher : MonoBehaviour
+    public class ProjectilLauncher : MonoBehaviour
     {
         [SerializeField] private Transform _firePosition;
         [SerializeField] private float _timeBetweenProjecteis;
 
         private Player _player;
         private GameObject _obj;
-        private Vector3 _direction = Vector3.zero;
+
+        private Vector3 _direction;
+        public Vector2 Direction { get { return _direction; } set {  _direction = value; } }
 
         public void Init(Player player)
         {
             _player = player;
+            _direction = Vector3.zero;
         }
 
         void Start()
@@ -32,13 +35,12 @@ namespace JAM.Projectils
 
         private void LaunchProjectil()
         {
-            Debug.Log("Projectil");
             _obj = CtrlAltJamGameManager.Instance.ProjectilPool.GetObject();
 
             IProjectil projectil = _obj.GetComponent<IProjectil>();
             if (projectil == null) return;
 
-            projectil.InitializeProjectil(_firePosition.position, _player.transform);
+            projectil.InitializeProjectil(_firePosition.position, _direction, _player.transform.rotation);
         }
     }
 }
